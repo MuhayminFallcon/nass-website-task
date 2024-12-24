d9c0bcddde01cd66c8dbe3db933b1cffe5dd5fb7
Muhaymin Fallcon <muhayminfallcon@gmail.com>
2024-12-24 21:14:45 +0300

    setup nuxt-vuetify-tailwind

---
+++ nuxt.config.ts
@@ -0,0 +1,26 @@
+import vuetify from 'vite-plugin-vuetify'
+import { transformAssetUrls } from 'vite-plugin-vuetify'
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  build: {
+    transpile: ['vuetify'],
+  },
+  modules: [
+    '@nuxtjs/tailwindcss',
+    (_options: any, nuxt: any) => {
+      nuxt.hooks.hook('vite:extendConfig', (config: any) => {
+        config.plugins.push(vuetify({ autoImport: true }))
+      })
+    },
+  ],
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
+    },
+  },
+})
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-container>
+    <h1>Welcome to Your Nuxt 3 Project</h1>
+  </v-container>
+  <v-alert color="secondary">This is a primary alert.</v-alert>
+</template>
+
+<script setup>
+</script>
